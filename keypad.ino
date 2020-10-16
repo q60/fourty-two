6849178bc470acfd51f5a4c68479ca5264c6dfd5
veleth <llathasa@outlook.com>
2020-10-16 22:09:20 +0300

    Update keypad.ino

--- keypad.ino
+++ keypad.ino
@@ -26,46 +26,36 @@ int _length;
 
 void (*reset_function) (void) = 0;
 
-void setup()
-{
+void setup() {
 	lcd.begin();
 	Serial.begin(9600);
 	lcd.backlight();
 	lcd.blink();
 }
 
-void loop()
-{
+void loop() {
   char customKey = customKeypad.getKey();
-	if (customKey)
-	{
-		switch (customKey)
-		{
-			case '#':
-			{
+	if (customKey) {
+		switch (customKey) {
+			case '#': {
 				lcd.clear();
 				_text.remove(_text.length() - 1);
-				if (_length > 0x10)
-				{
+				if (_length > 0x10) {
 					_length--;
 					_text_well_formed = _text.substring(_erase_counter, _text.length());
-					if (_height == 1)
-					{
+					if (_height == 1) {
 						String _text_first_part = _text.substring((_erase_counter - 0x10), _erase_counter);
 						lcd.print(_text_first_part);
 					}
 					lcd.setCursor(0, _height);
 					lcd.print(_text_well_formed);
-				}
-				else
-				{
+				} else {
 					lcd.print(_text);
 				}
 				break;
 			}
 
-			case '*':
-			{
+			case '*': {
 				_text.getBytes(buf, 0x400, 0);
 				lcd.clear();
 				lcd.setCursor(0, 0);
@@ -73,8 +63,7 @@ void loop()
 				break;
 			}
 
-			case 'C':
-			{
+			case 'C': {
 				lcd.clear();
 				lcd.print("Reloading...");
 				delay(500);
@@ -82,22 +71,17 @@ void loop()
 				break;
 			}
 
-			default:
-			{
+			default: {
 				lcd.print(customKey);
 				_text += customKey;
 				_counter_length = _text.length();
 				_length++;
-				if(_counter_length >= 0x10)
-				{
-					if (_counter_length % 0x10 == 0 && round((_counter_length / 16.0) - 0.5) % 2 != 0)
-					{
+				if(_counter_length >= 0x10) {
+					if (_counter_length % 0x10 == 0 && round((_counter_length / 16.0) - 0.5) % 2 != 0) {
 						lcd.setCursor(0, 1);
 						_erase_counter += 0x10;
 						_height = 1;
-					}
-					else if (_counter_length % 0x20 == 0)
-					{
+					} else if (_counter_length % 0x20 == 0) {
 						lcd.clear();
 						lcd.setCursor(0, 0);
 						_erase_counter += 0x10;
@@ -110,18 +94,15 @@ void loop()
 	}
 }
 
-void brainfuck(char code[])
-{
+void brainfuck(char code[]) {
 	int code_position = 0;
 	byte memory[0xFF] = {0};
 	byte memory_position;
 	int stack[0xFF] = {0};
 	byte stack_position = 0;
 
-	while (code[code_position] != 0)
-	{
-		switch (code[code_position])
-		{
+	while (code[code_position] != 0) {
+		switch (code[code_position]) {
 			case '>':
 				memory_position++;
 				break;
@@ -143,10 +124,8 @@ void brainfuck(char code[])
 				break;
 
 			case ',':
-				for (;;)
-				{
-					if (Serial.available() > 0)
-					{
+				for (;;) {
+					if (Serial.available() > 0) {
 						memory[memory_position] = Serial.read();
 						break;
 					}
@@ -154,25 +133,22 @@ void brainfuck(char code[])
 				break;
 
 			case '[':
-				if (memory[memory_position])
-				{
+				if (memory[memory_position]) {
 					stack_position++;
 					stack[stack_position] = code_position;
-				}
-				else
-				{
-					while(code[code_position] != ']')
-					{
+				} else {
+					while(code[code_position] != ']') {
 						code_position++;
 					}
 				}
 				break;
 
 			case ']':
-				if (memory[memory_position])
+				if (memory[memory_position]) {
 					code_position = stack[stack_position];
-				else
+				} else {
 					stack_position--;
+				}
 				break;
 		}
 		code_position++;
